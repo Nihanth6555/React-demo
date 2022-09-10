d2765c41dfcbd3c7242b0d7df7904fd3282a237b
Niahnth <b.nihanthreddy6555@gmail.com>
2022-09-10 12:03:57 -0600

    two changes

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react course/i);
+  const linkElement = screen.getByText(/learn react code/i);
   expect(linkElement).toBeInTheDocument();
 });
